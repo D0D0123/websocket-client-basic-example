4f81a0927fcfd6ee8c7441bd2d2e47c1d44c4068
Soura Mandal <souramandal@pop-os.localdomain>
2023-06-04 20:21:18 +1000

    documented example

---
+++ README.md
@@ -0,0 +1,36 @@
+# Basic Websocketpp Client Example
+
+This is a basic example of a websocketpp client, which maintains a single connection to a websocket server. It waits for user input and sends it to the server in a loop.
+
+If the connection to the server is lost, it will attempt to reconnect upon every subsequent message. If the connection is reestablished, it will resume sending messages.
+
+It is intended to be a very barebones example derived from the [full example](https://github.com/zaphoyd/websocketpp/blob/master/tutorials/utility_client/step6.cpp) on the websocketpp website. It does not contain any handlers or other features.
+
+See the comments in main.cpp for more information.
+
+See the following for other useful examples and explanation:
+ - https://github.com/zaphoyd/websocketpp/blob/master/tutorials/utility_client/utility_client.md
+ - https://gist.github.com/zaphoyd/6021634
+
+## Building and Running
+
+Install wscat with `npm install -g wscat`.
+
+To build the `ws_test` executable, run the following from the root of the repo:
+```bash
+cmake .
+make
+```
+
+Then in one terminal run
+```bash
+wscat --listen 12000
+```
+
+and in another run
+```bash
+./ws_test
+```
+
+To test reconnect functionality, kill the wscat process and restart it. The client should reconnect upon the next attempt to send a message and resume sending messages.
+
--- main.cpp
+++ main.cpp
@@ -12,19 +12,29 @@ typedef websocketpp::client<websocketpp::config::asio_client> client;
 
 class basic_ws_client {
 public:
-    basic_ws_client(std::string uri) {
+    basic_ws_client(std::string uri): m_uri(uri) {
+        /* These disable logs from the websocketpp library. 
+           Comment these two lines out if you wish to see them, although it will make the terminal output cluttered. 
+        */
         m_endpoint.clear_access_channels(websocketpp::log::alevel::all);
         m_endpoint.clear_error_channels(websocketpp::log::elevel::all);
 
+        /* Initialize the transport system underlying the endpoint and set it to perpetual mode. 
+           In perpetual mode the endpoint's processing loop will not exit automatically when it has no connections / disconnects.
+           This is necessary if we are trying to implement reconnects
+        */ 
         m_endpoint.init_asio();
         m_endpoint.start_perpetual();
 
-        m_uri = uri;
-
+        /* Starts a thread that will run the endpoint's processing loop. 
+           This will allow the endpoint to process incoming messages and dispatch them to the appropriate handlers.
+           This needs to run in a separate thread because it is a blocking call.
+        */
         m_thread = websocketpp::lib::make_shared<websocketpp::lib::thread>(&client::run, &m_endpoint);
     }
 
     ~basic_ws_client() {
+        // the destructor will stop the endpoint's processing loop and join the thread
         m_endpoint.stop_perpetual();
         m_thread->join();
     }
@@ -35,21 +45,39 @@ public:
         return err;
     }
 
+    /**
+     * @brief Try to connect to the server. If successful, the new connection handle will be stored in m_connection_hdl.
+     */
     void try_connect() {
         websocketpp::lib::error_code err_code;
         client::connection_ptr connection = m_endpoint.get_connection(m_uri, err_code);
         if (err_code) {
             std::cerr << "Error connecting: " << err_code.message() << std::endl;
+            return;
         }
 
+        // websocketpp uses handle objects to track connections. Connection pointers should not be directly used after calling connect.
+        // See https://github.com/zaphoyd/websocketpp/blob/master/tutorials/utility_client/utility_client.md#terminology-connection_hdl
         m_connection_hdl = connection->get_handle();
         m_endpoint.connect(connection);
     }
 
 private:
+    /**
+     * The websocketpp endpoint object.
+     */
     client m_endpoint;
+    /**
+     * The current connection handle.
+     */
     websocketpp::connection_hdl m_connection_hdl;
+    /**
+     * The thread that will run the endpoint's processing loop.
+     */
     websocketpp::lib::shared_ptr<websocketpp::lib::thread> m_thread;
+    /**
+     * The URI of the websocket server.
+     */
     std::string m_uri;
 
 };
@@ -60,10 +88,12 @@ int main() {
     ws.try_connect();
 
     std::string msg;
-    while (1) {
+    while (true) {
         std::cout << "Write Message:" << std::endl;
         std::cin >> msg;
+        
         websocketpp::lib::error_code err = ws.send(msg);
+
         if (err) {
             std::cerr << "Could not send message, disconnected from websocket server.\nAttempting to Reconnect." << std::endl;
             ws.try_connect();
